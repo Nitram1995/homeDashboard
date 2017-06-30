86f3a6d3d9b762dda40f37911159567aa338f54b
Martin Jensen <martin.jensen.1995@gmail.com>
2017-06-30 21:27:26 +0200

    Added explanitory comments to udp client

--- modules/udp_client.py
+++ modules/udp_client.py
@@ -1,16 +1,15 @@
 import socket
 
 #Standard Project CARS specifications
-host = ''
+host = '' #Symbolic value (will figure it out by itself)
 port = 5606
-exp_length = 1367
+exp_length = 1367 #Expected amount of bytes per package
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Setting up object with UDP protocol
 s.bind((host, port))
 
 def setup_client(newHost, newPort, newExpectedLength):
 	global s	
-
 	global host
 	global port 
 	global exp_length 
@@ -24,4 +23,9 @@ def setup_client(newHost, newPort, newExpectedLength):
 
 
 def get_udp_data():
-	return s.recv(exp_length)
+	'''
+	Getting data on the network.
+	Currently no time-out, 
+		so it waits here until something is recieved
+	'''
+	return s.recv(exp_length) 
