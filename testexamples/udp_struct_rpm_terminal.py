e9fc587e38896521c22b838e4bf63f86d716f870
Martin <Martin.Jensen.1995@gmail.com>
2017-06-30 23:07:23 +0200

    Added gear and rpm test with use of struct modules to unpack data

---
+++ testexamples/udp_struct_rpm_terminal.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append("/home/pi/Git/homeDashboard/modules")
+
+import udp_client as udp
+import struct
+
+
+#Sets up client for Project Cars specs
+udp.setup_client('', 5606, 1367)
+
+while True:
+        data = udp.get_udp_data()
+	gear = struct.unpack("<b", data[128])[0] & 0xf0 >> 4
+	rpm = struct.unpack("<H", data[124] + data[125])
+	print gear, rpm[0]
+        
+
