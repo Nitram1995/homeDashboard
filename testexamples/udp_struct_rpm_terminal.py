2d5a737d0afff0bf517eb090a0c02d23fa13f337
Martin Jensen <martin.jensen.1995@gmail.com>
2017-07-01 13:47:49 +0200

    Added to-do list and modified struct test

--- testexamples/udp_struct_rpm_terminal.py
+++ testexamples/udp_struct_rpm_terminal.py
@@ -9,9 +9,13 @@ import struct
 udp.setup_client('', 5606, 1367)
 
 while True:
-        data = udp.get_udp_data()
+	data = udp.get_udp_data()
+	'''
 	gear = struct.unpack("<b", data[128])[0] & 0xf0 >> 4
 	rpm = struct.unpack("<H", data[124] + data[125])
-	print gear, rpm[0]
+	'''
+	speed, rpm, maxRpm = struct.unpack("<fHH", data[120:128])
+
+	print speed, rpm, maxRpm
         
 
