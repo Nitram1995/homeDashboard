eb1a576e7290f3a5ddd06d261e4fb0f79aa5e080
Martin Jensen <martin.jensen.1995@gmail.com>
2017-09-26 16:25:13 +0200

    All sections of the GUI are now their own frames

--- modules/GUI.py
+++ modules/GUI.py
@@ -1,15 +1,16 @@
 import tkinter as tk
 import tkinter.font as tkFont
 
-bg_color = "black"
-border_color = "white"
-txt_color = "white"
-header_color = "yellow"
-
-main_font = "helvetica"
+bg_color = 'black'
+border_color = 'white'
+txt_color = 'white'
+header_color = 'yellow'
 
 
 root = tk.Tk()
+
+main_font = tkFont.Font(family='Helvetica', size=12, weight='bold')
+small_font = tkFont.Font(family='Helvetica', size=int(main_font['size']*2), weight='bold')
 
 gear_num = tk.StringVar()
 
@@ -43,8 +44,13 @@ def update_variables(gData):
 	fuel.set('{:.2f}'.format(gData.curr_fuel()))
 
 def quit(event):
-	import sys
-	sys.exit()
+	root.quit()
+	root.destroy()
+	#import sys
+	#sys.exit()
+
+def get_window_dimensions():
+	return root.winfo_screenwidth(), root.winfo_screenheight()
 
 def root_setup(root):
 	root.bind('<Escape>', quit)
@@ -54,9 +60,9 @@ def root_setup(root):
 	tk.Grid.columnconfigure(root, 0, weight=1)
 
 	# Make it cover the entire screen (http://effbot.org/zone/tkinter-toplevel-fullscree$
-	w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+	w, h = get_window_dimensions()
 	root.overrideredirect(1)
-	root.geometry("%dx%d+0+0" % (w, h))
+	root.geometry('%dx%d+0+0' % (w, h))
 
 	#root.focus_set() # <-- move focus to this widget
 
@@ -64,98 +70,152 @@ class screenPCars:
 	def __init__(self, master):
 		frame = tk.Frame(master, borderwidth=1, bg=bg_color, highlightbackground=border_color, highlightthickness=1, relief=tk.SOLID, bd=0) #width=master.width, height=master.height,
 		frame.grid(row=0, column=0, sticky='nsew')
-
+		
 		self.relief = tk.GROOVE
 		self.font = main_font
-		self.font = tkFont.Font(size = 15)
-
+		
 		#Configuring the grid in the frame
-		for row_index in range(9):
-			tk.Grid.rowconfigure(frame, row_index, weight=1)
-			for col_index in range(16):
-				tk.Grid.columnconfigure(frame, col_index, weight=1)
-				#lbl = tk.Label(frame, text=str(col_index), font=self.font, bg=bg_color, fg=txt_color, borderwidth=1, highlightbackground=border_color, highlightthickness=1, relief=self.relief)
-				#lbl.grid(row=row_index, column=col_index, sticky='nsew')
+		configure_grid(frame, 9, 16)
 
-		#Setting up labels
-		lbl_gearNum_header = tk.Label(frame, text="Gear", font=self.font, bg=bg_color, fg=header_color, borderwidth=2)
-		lbl_gearNum = tk.Label(frame, textvariable=gear_num, font=self.font, bg=bg_color, fg=txt_color, borderwidth=1, relief=self.relief)
+		fuel_frame = self.make_fuel_frame(frame)
+		fuel_frame.grid(row=0, column=0, rowspan=5, columnspan=3, sticky='nsew')
+		
+		water_t_frame = self.make_water_temperature_frame(frame)
+		water_t_frame.grid(row=0, column=13, rowspan=3, columnspan=3, sticky='nsew')
+		oil_t_frame = self.make_oil_temperature_frame(frame)
+		oil_t_frame.grid(row=3, column=13, rowspan=3, columnspan=3, sticky='nsew')
 
-		lbl_water_header = tk.Label(frame, text="Water T", font=self.font, bg=bg_color, fg=header_color, borderwidth=2)
-		lbl_water_temp = tk.Label(frame, textvariable=water_temp, font=self.font, bg=bg_color, fg=txt_color, borderwidth=1, relief=self.relief)
-		lbl_oil_header = tk.Label(frame, text="Oil T", font=self.font, bg=bg_color, fg=header_color, borderwidth=2)
-		lbl_oil_temp = tk.Label(frame, textvariable=oil_temp, font=self.font, bg=bg_color, fg=txt_color, borderwidth=1, relief=self.relief)
+		engine_dmg_frame = self.make_engine_damage_frame(frame)
+		engine_dmg_frame.grid(row=5, column=0, rowspan=2, columnspan=3, sticky='nsew')
+		aero_dmg_frame = self.make_aero_damage_frame(frame)
+		aero_dmg_frame.grid(row=7, column=0, rowspan=2, columnspan=3, sticky='nsew')
 
-		lbl_aero_header = tk.Label(frame, text="Aero Dmg", font=self.font, bg=bg_color, fg=header_color, borderwidth=2)
-		lbl_aero_dmg = tk.Label(frame, textvariable=aero_dmg, font=self.font, bg=bg_color, fg=txt_color, borderwidth=1, relief=self.relief)
-		lbl_engine_header = tk.Label(frame, text="Eng Dmg", font=self.font, bg=bg_color, fg=header_color, borderwidth=2)
-		lbl_engine_dmg = tk.Label(frame, textvariable=engine_dmg, font=self.font, bg=bg_color, fg=txt_color, borderwidth=1, relief=self.relief)
+		tire_temps_frame = self.make_tire_temperature_frame(frame)
+		tire_temps_frame.grid(row=6, column=10, rowspan=3, columnspan=6, sticky='nsew')
 
-		lbl_fuel_header = tk.Label(frame, text="Fuel", font=self.font, bg=bg_color, fg=header_color, borderwidth=2)
-		lbl_fuel = tk.Label(frame, textvariable=fuel, font=self.font, bg=bg_color, fg=txt_color, borderwidth=1, relief=self.relief)
-		lbl_fuel_remain_header = tk.Label(frame, text="Laps left", font=self.font, bg=bg_color, fg=header_color, borderwidth=2)
-		lbl_fuel_remain = tk.Label(frame, textvariable=fuel_laps_remain, font=self.font, bg=bg_color, fg=txt_color, borderwidth=1, relief=self.relief)
+		gear_frame = self.make_gear_frame(frame)
+		gear_frame.grid(row=0, column=8, rowspan=6, columnspan=2, sticky='nsew')
 
-		lbl_tire_temp_header = tk.Label(frame, text="Tire temps", font=self.font, bg=bg_color, fg=header_color, borderwidth=2)
+		#frame.bind('<Configure>', self.resize)
+
+		resizeFont()
+
+	def make_tire_temperature_frame(self, master):
+		frame = tk.Frame(master, borderwidth=1, bg=bg_color, highlightbackground=border_color, highlightthickness=1, relief=tk.SOLID, bd=0)
+		configure_grid(frame, 3, 6)
+
+		lbl_tire_temp_header = tk.Label(frame, text='Tire temps', font=self.font, bg=bg_color, fg=header_color, borderwidth=2)
 		lbl_fl_tyre_temp = tk.Label(frame, textvariable=fl_tire_temp, font=self.font, bg=bg_color, fg=txt_color, borderwidth=1, relief=self.relief)
 		lbl_fr_tyre_temp = tk.Label(frame, textvariable=fr_tire_temp, font=self.font, bg=bg_color, fg=txt_color, borderwidth=1, relief=self.relief)
 		lbl_rl_tyre_temp = tk.Label(frame, textvariable=rl_tire_temp, font=self.font, bg=bg_color, fg=txt_color, borderwidth=1, relief=self.relief)
 		lbl_rr_tyre_temp = tk.Label(frame, textvariable=rr_tire_temp, font=self.font, bg=bg_color, fg=txt_color, borderwidth=1, relief=self.relief)
 
+		lbl_tire_temp_header.grid(row=0, column=0, columnspan=6, sticky='nsew')
+		lbl_fl_tyre_temp.grid(row=1, column=0, rowspan=1, columnspan=3, sticky='nsew')
+		lbl_fr_tyre_temp.grid(row=1, column=3, rowspan=1, columnspan=3, sticky='nsew')
+		lbl_rl_tyre_temp.grid(row=2, column=0, rowspan=1, columnspan=3, sticky='nsew')
+		lbl_rr_tyre_temp.grid(row=2, column=3, rowspan=1, columnspan=3, sticky='nsew')
 
-		#Gridding the labels
-		lbl_gearNum_header.grid(row=0, column=8, columnspan=2, sticky='nsew')
-		lbl_gearNum.grid(row=1, column=8, rowspan=5, columnspan=2, sticky='nsew')
+		return frame
 
-		lbl_water_header.grid(row=0, column=13, columnspan=3, sticky='nsew')
-		lbl_water_temp.grid(row=1, column=13, rowspan=2, columnspan=3, sticky='nsew')
-		lbl_oil_header.grid(row=3, column=13, columnspan=3, sticky='nsew')
-		lbl_oil_temp.grid(row=4, column=13, rowspan=2, columnspan=3, sticky='nsew')
+	def make_fuel_frame(self, master):
+		frame = tk.Frame(master, borderwidth=1, bg=bg_color, highlightbackground=border_color, highlightthickness=1, relief=tk.SOLID, bd=0)
+		configure_grid(frame, 5, 3)
 
-		lbl_aero_header.grid(row=0, column=10, columnspan=3, sticky='nsew')
-		lbl_aero_dmg.grid(row=1, column=10, rowspan=2, columnspan=3, sticky='nsew')
-		lbl_engine_header.grid(row=3, column=10, columnspan=3, sticky='nsew')
-		lbl_engine_dmg.grid(row=4, column=10, rowspan=2, columnspan=3, sticky='nsew')
+		lbl_fuel_header = tk.Label(frame, text='Fuel', font=self.font, bg=bg_color, fg=header_color, borderwidth=2)
+		lbl_fuel = tk.Label(frame, textvariable=fuel, font=self.font, bg=bg_color, fg=txt_color, borderwidth=1, relief=self.relief)
+		lbl_fuel_remain_header = tk.Label(frame, text='Laps left', font=self.font, bg=bg_color, fg=header_color, borderwidth=2)
+		lbl_fuel_remain = tk.Label(frame, textvariable=fuel_laps_remain, font=self.font, bg=bg_color, fg=txt_color, borderwidth=1, relief=self.relief)
 
 		lbl_fuel_header.grid(row=0, column=0, columnspan=3, sticky='nsew')
 		lbl_fuel.grid(row=1, column=0, rowspan=2, columnspan=3, sticky='nsew')
 		lbl_fuel_remain.grid(row=3, column=0, rowspan=2, columnspan=3, sticky='nsew')
 
-		lbl_tire_temp_header.grid(row=6, column=10, columnspan=6, sticky='nsew')
-		lbl_fl_tyre_temp.grid(row=7, column=10, rowspan=1, columnspan=3, sticky='nsew')
-		lbl_fr_tyre_temp.grid(row=7, column=13, rowspan=1, columnspan=3, sticky='nsew')
-		lbl_rl_tyre_temp.grid(row=8, column=10, rowspan=1, columnspan=3, sticky='nsew')
-		lbl_rr_tyre_temp.grid(row=8, column=13, rowspan=1, columnspan=3, sticky='nsew')
+		return frame
 
-		self.labels = [lbl_gearNum_header, lbl_gearNum, lbl_water_header, lbl_water_temp, 
-		lbl_oil_header, lbl_oil_temp, lbl_aero_header, lbl_aero_dmg, lbl_engine_header, lbl_engine_dmg,
-		lbl_fuel_header, lbl_fuel, lbl_fuel_remain, lbl_tire_temp_header,
-		lbl_fl_tyre_temp, lbl_fr_tyre_temp, lbl_rl_tyre_temp, lbl_rr_tyre_temp]
 
-		for lbl in self.labels:
-			resizeFont(lbl)
+	def make_aero_damage_frame(self, master):
+		frame = tk.Frame(master, borderwidth=1, bg=bg_color, highlightbackground=border_color, highlightthickness=1, relief=tk.SOLID, bd=0)
+		configure_grid(frame, 2, 3)
 
-		#frame.bind('<Configure>', self.resize)
+		lbl_aero_header = tk.Label(frame, text='Aero Dmg', font=self.font, bg=bg_color, fg=header_color, borderwidth=2)
+		lbl_aero_dmg = tk.Label(frame, textvariable=aero_dmg, font=self.font, bg=bg_color, fg=txt_color, borderwidth=1, relief=self.relief)
+
+		lbl_aero_header.grid(row=0, column=0, columnspan=3, sticky='nsew')
+		lbl_aero_dmg.grid(row=1, column=0, columnspan=3, sticky='nsew')
+
+		return frame
+
+
+	def make_engine_damage_frame(self, master):
+		frame = tk.Frame(master, borderwidth=1, bg=bg_color, highlightbackground=border_color, highlightthickness=1, relief=tk.SOLID, bd=0)
+		configure_grid(frame, 2, 3)
+
+		lbl_engine_header = tk.Label(frame, text='Eng Dmg', font=self.font, bg=bg_color, fg=header_color, borderwidth=2)
+		lbl_engine_dmg = tk.Label(frame, textvariable=engine_dmg, font=self.font, bg=bg_color, fg=txt_color, borderwidth=1, relief=self.relief)
+
+		lbl_engine_header.grid(row=0, column=0, columnspan=3, sticky='nsew')
+		lbl_engine_dmg.grid(row=1, column=0, columnspan=3, sticky='nsew')
+
+		return frame
+
+
+	def make_water_temperature_frame(self, master):
+		frame = tk.Frame(master, borderwidth=1, bg=bg_color, highlightbackground=border_color, highlightthickness=1, relief=tk.SOLID, bd=0)
+		configure_grid(frame, 3, 3)
+
+		lbl_water_header = tk.Label(frame, text='Water T', font=self.font, bg=bg_color, fg=header_color, borderwidth=2)
+		lbl_water_temp = tk.Label(frame, textvariable=water_temp, font=self.font, bg=bg_color, fg=txt_color, borderwidth=1, relief=self.relief)
+
+		lbl_water_header.grid(row=0, column=0, columnspan=3, sticky='nsew')
+		lbl_water_temp.grid(row=1, column=0, rowspan=2, columnspan=3, sticky='nsew')
+
+		return frame
+
+
+	def make_oil_temperature_frame(self, master):
+		frame = tk.Frame(master, borderwidth=1, bg=bg_color, highlightbackground=border_color, highlightthickness=1, relief=tk.SOLID, bd=0)
+		configure_grid(frame, 3, 3)
+
+		lbl_oil_header = tk.Label(frame, text='Oil T', font=self.font, bg=bg_color, fg=header_color, borderwidth=2)
+		lbl_oil_temp = tk.Label(frame, textvariable=oil_temp, font=self.font, bg=bg_color, fg=txt_color, borderwidth=1, relief=self.relief)
+
+		lbl_oil_header.grid(row=0, column=0, columnspan=3, sticky='nsew')
+		lbl_oil_temp.grid(row=1, column=0, rowspan=2, columnspan=3, sticky='nsew')
+
+		return frame
+
+
+	def make_gear_frame(self, master):
+		frame = tk.Frame(master, borderwidth=1, bg=bg_color, highlightbackground=border_color, highlightthickness=1, relief=tk.SOLID, bd=0)
+		configure_grid(frame, 6, 1)
+
+		lbl_gear_header = tk.Label(frame, text='Gear', font=self.font, bg=bg_color, fg=header_color, borderwidth=2)
+		lbl_gear_num = tk.Label(frame, textvariable=gear_num, font=self.font, bg=bg_color, fg=txt_color, borderwidth=1, relief=self.relief)
+
+		lbl_gear_header.grid(row=0, column=0, sticky='nsew')
+		lbl_gear_num.grid(row=1, column=0, rowspan=5, sticky='nsew')
+
+		return frame
+
 
 	def resize(self, event):
 			pass
 			#resizeFont(self.lbl_fuel_remain)
 
-def resizeFont(lbl):
-	lblHeight = root.winfo_height()
-	lblFont = tkFont.Font(size = 50)
+def resizeFont():
+	winW, winH = get_window_dimensions()
+
+	main_font.configure(size=40)
+
+	'''
+	lblFont = tkFont.Font(family='Helvetica', size=50, weight='bold')
 	lbl.configure(font=lblFont)
 	print (lblHeight)
+	'''
 
-
-
-
-'''
-lbl_gearNum = tk.Label(root, textvariable=num, font=("System", 70, "bold"), fg="white", bg="$
-
-lbl_gearNum.grid(row=0, column=0)
-
-gui_setup()
-root.after(500, method)
-root.mainloop()
-'''
+def configure_grid(frame, rows, columns):
+	for row_index in range(rows):
+		tk.Grid.rowconfigure(frame, row_index, weight=1)
+		for col_index in range(columns):
+			tk.Grid.columnconfigure(frame, col_index, weight=1)
